b53c357659d12f7487b20ba6ad0221d06c082c5f
TheMrPhantom <schieljn@studi.informatik.uni-stuttgart.de>
2019-04-11 01:37:03 +0200

    Endpoint listening working

--- FIUSInfo/FIUSInfo.ino
+++ FIUSInfo/FIUSInfo.ino
@@ -2,17 +2,17 @@
 #include <WiFi.h>
 
 
-#define LED_WIDTH 0
-#define LED_HEIGTH 0
-#define NUM_LEDS LED_WIDTH*LED_HEIGTH
+const int LED_WIDTH = 32;
+const int LED_HEIGTH = 8;
+const int NUM_LEDS = LED_WIDTH * LED_HEIGTH;
+
 //Messing around with DATA_PIN can cause compile problems due library name collision
-#define DATA_PIN 4
+const int DATA_PIN = 4;
 
 CRGB leds[NUM_LEDS];
 
 char ssid[] = "yourNetwork";      // your network SSID (name)
 char pass[] = "secretPassword";   // your network password
-int keyIndex = 0;                 // your network key Index number (needed only for WEP)
 
 int status = WL_IDLE_STATUS;
 
@@ -20,24 +20,30 @@ WiFiServer server(80);
 
 void setup() {
   FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
-  
+  Serial.begin(9600);
   setupWlan();
-
+  leds[10] = CRGB::White; FastLED.show();
 }
 
 void loop() {
   // listen for incoming clients
   refreshPage();
-  refreshLED();
+  //refreshLED();
 }
 
-void setupWlan(){
-   // attempt to connect to Wifi network:
+void setupWlan() {
+  WiFi.softAP(ssid, "");
+  server.begin();
+  return;
+  // atparsingStringt to connect to Wifi network:
   while (status != WL_CONNECTED) {
-    Serial.print("Attempting to connect to SSID: ");
+    Serial.print("AtparsingStringting to connect to SSID: ");
     Serial.println(ssid);
     // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
     status = WiFi.begin(ssid, pass);
+
+
+
 
     // wait 10 seconds for connection:
     delay(5000);
@@ -45,14 +51,31 @@ void setupWlan(){
   server.begin();
 }
 
-void refreshPage(){
-   WiFiClient client = server.available();
+void refreshPage() {
+  WiFiClient client = server.available();
+  bool check = false;
   if (client) {
+    String parsingString = "";
+
     // an http request ends with a blank line
-    boolean currentLineIsBlank = true;
+    bool currentLineIsBlank = true;
     while (client.connected()) {
       if (client.available()) {
         char c = client.read();
+        parsingString += c;
+        int startIndex = parsingString.indexOf("GET");
+      //  Serial.print(c);
+        if (parsingString.lastIndexOf("HTTP/1.1") != -1 && !check) {
+          check = true;
+
+          int endIndex = parsingString.lastIndexOf("HTTP/1.1");
+          parsingString = parsingString.substring(startIndex, endIndex);
+          parsingString = parsingString.substring(5, parsingString.length() - 1);
+          if (parsingString.indexOf("favicon.ico") == -1) {
+            reactOnHTTPCall(parsingString);
+          }
+
+        }
         // if you've gotten to the end of the line (received a newline
         // character) and the line is blank, the http request has ended,
         // so you can send a reply
@@ -62,10 +85,10 @@ void refreshPage(){
           client.println("Content-Type: text/html");
           client.println("Connection: close");  // the connection will be closed after completion of the response
           client.println();
+
           client.println("<!DOCTYPE HTML>");
           client.println("<html>");
-          // output the value of each analog input pin
-          
+
           client.println("OK");
 
           client.println("</html>");
@@ -82,12 +105,16 @@ void refreshPage(){
     }
     // give the web browser time to receive the data
     delay(1);
-
     // close the connection:
     client.stop();
+
   }
 }
 
-void refreshLED(){
-  
+void reactOnHTTPCall(String message) {
+  Serial.println(message);
+}
+
+void refreshLED() {
+
 }
